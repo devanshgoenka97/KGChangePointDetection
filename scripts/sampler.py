a4bf3d25951221fa22dee01df24e363a60d49676
Devansh Goenka <devanshgoenka97@gmail.com>
2022-11-05 14:50:55 -0500

    Add comments

--- scripts/sampler.py
+++ scripts/sampler.py
@@ -13,15 +13,18 @@ if len(sys.argv) <= 1:
     print("Number of change points not specified")
     exit()
 
+print("Processing input data...")
 with open('../FB15K-237/train.txt', 'r') as f:
     reader = csv.reader(f, delimiter='\t')
 
-    for row in reader:
+    for row in tqdm(reader):
         (head, relation, tail) = row
         ENTITIES.add(head)
         ENTITIES.add(tail)
         RELATIONS.add(relation)
         TRIPLETS.append((head, relation, tail))
+
+print("Done processing input data.")
 
 no_entities = len(ENTITIES)
 no_relations = len(RELATIONS)
@@ -38,6 +41,9 @@ change_points = int(sys.argv[1])
 entities_in_subgraph = no_entities // change_points
 relations_in_subgraph = no_relations // change_points
 
+
+print("Generating synthetic KGs for change points...")
+
 # For each change point, sample indices uniformly
 for i in range(change_points):
     entity_indices = np.random.randint(no_entities, size=entities_in_subgraph)
@@ -47,8 +53,12 @@ for i in range(change_points):
     entity_subgraph = [ENTITIES[idx] for idx in entity_indices]
     relation_subgraph = [RELATIONS[idx] for idx in relation_indices]
 
+    print(f"Generating KG for change point {i}...")
+
     for (head, relation, tail) in tqdm(TRIPLETS):
         # All entities and relations in this triplet belong to the 
         if head in entity_subgraph and tail in entity_subgraph and relation in relation_subgraph:
             pass
             # Save this triplet to a new file for the dataset
+
+print("Done generating synthetic KGs.")
