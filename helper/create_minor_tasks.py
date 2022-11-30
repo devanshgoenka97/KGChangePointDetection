32bac027f9806c292857b1b1c1f470e847627254
Devansh Goenka <devanshgoenka97@gmail.com>
2022-11-30 00:02:46 -0600

    Update create minor task to create random changed graphs

--- helper/create_minor_tasks.py
+++ helper/create_minor_tasks.py
@@ -3,12 +3,22 @@ import numpy as np
 import csv
 import sys
 
+# Set seed to create/re-create shuffled data
+np.random.seed(1234)
+
 # Initializing as set to get unique entities and relations
 ENTITIES = set()
 RELATIONS = set()
 TRIPLETS = []
 
-SUBGRAPH_CHANGE_PERCENT = 0.5
+DEFAULT_CHANGE_PERCENT = 1.0
+INITIAL_COOLDOWN = 10
+OTHER_CHANGE_PERCENTS = [2.0, 5.0, 10.0]
+
+if len(sys.argv) < 2:
+    print("Error: Expected number of timesteps in argument")
+    exit()
+
 timesteps = int(sys.argv[1])
 
 # Read original entities and relations in test set
@@ -53,43 +63,43 @@ maj_triplets = len(MAJOR_TRIPLETS)
 MAJOR_ENTITIES = list(MAJOR_ENTITIES)
 MAJOR_RELATIONS = list(MAJOR_RELATIONS)
 
-
 # Function to store triplets to disk
-def store_triplets(triplets, is_major, timestep=None):
-    timestep_name = f"timestep_{timestep}_{SUBGRAPH_CHANGE_PERCENT}_change" if not is_major else "major"
+def store_triplets(triplets, change, timestep=None):
+    timestep_name = f"timestep_{timestep}_{change}_change"
     filename = f"test_{timestep_name}.txt"
-    
 
     with open('./testdata/' + filename, 'w') as f:
         writer = csv.writer(f, delimiter ='\t',quotechar =',',quoting=csv.QUOTE_MINIMAL)
         for (head, relation, tail) in triplets:
             writer.writerow([head, relation, tail])
 
+
 # Now for each timestep, perform bounded addition and deletion and create sub test graphs
 for i in range(timesteps):
-    print(f"Generating KG for timestep {i}...")
-    entities_add = int(maj_entities * (SUBGRAPH_CHANGE_PERCENT/100.0))
-    relations_add = int(maj_relations * (SUBGRAPH_CHANGE_PERCENT/100.0))
+    print(f"Generating KG for timestep {i+1}...")
+    change = DEFAULT_CHANGE_PERCENT
+
+    # Increase change percent to create change point
+    if i + 1 > INITIAL_COOLDOWN and np.random.uniform() <= 0.15:
+        change = np.random.choice(OTHER_CHANGE_PERCENTS, size=1)[0]
+
+    entities_add = int(maj_entities * (change/100.0))
     entities_sub = entities_add
-    relations_sub = relations_add
 
     # Randomly drop entities_sub and relations_sub number from their respective subgraphs
     MINOR_ENTITIES = list(np.random.choice(MAJOR_ENTITIES, size = maj_entities - entities_sub))
-    MINOR_RELATIONS = list(np.random.choice(MAJOR_RELATIONS, size = maj_relations - relations_sub))
 
     non_contributing_entities = [e for e in ENTITIES if e not in MAJOR_ENTITIES]
-    non_contributing_relations = [r for r in RELATIONS if r not in MAJOR_RELATIONS]
 
     # Add some entities and relations from non-used ones
     MINOR_ENTITIES.extend(np.random.choice(non_contributing_entities, size = entities_add))
-    MINOR_RELATIONS.extend(np.random.choice(non_contributing_relations, size = relations_add))
 
     MINOR_TASK_TRIPLETS = []
     for (head, relation, tail) in tqdm(TRIPLETS):
         # All entities and relations in this triplet belong to the 
-        if head in MINOR_ENTITIES and tail in MINOR_ENTITIES and relation in MINOR_RELATIONS:
+        if head in MINOR_ENTITIES and tail in MINOR_ENTITIES:
             MINOR_TASK_TRIPLETS.append((head, relation, tail))
 
-    store_triplets(MINOR_TASK_TRIPLETS, False, i+1)
+    store_triplets(MINOR_TASK_TRIPLETS, change, i+1)
 
 print("Done generating synthetic KGs.")
