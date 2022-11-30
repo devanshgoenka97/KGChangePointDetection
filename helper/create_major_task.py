ff6739b82fa5111e5cee02b03963e24429dedcab
Devansh Goenka <devanshgoenka97@gmail.com>
2022-11-29 22:43:08 -0600

    Create different training data with 95% entities and all relations

--- helper/create_major_task.py
+++ helper/create_major_task.py
@@ -4,40 +4,68 @@ import csv
 import sys
 
 # Initializing as set to get unique entities and relations
-TRAIN_ENTITIES = set()
-TRAIN_RELATIONS = set()
-TRAIN_TRIPLETS = []
+ENTITIES = set()
+RELATIONS = set()
+TRIPLETS = []
 
-with open('./data/FB15K-237/train.txt', 'r') as f:
+if len(sys.argv) < 2:
+    print("Error: Need argument as the subgraph change percent for the major task")
+    exit()
+
+SUBGRAPH_CHANGE_PERCENT = int(sys.argv[1])
+
+# Creating major task train and test sets
+print("Processing input data...")
+
+with open('./data/FB15K-237/original_train.txt', 'r') as f:
     reader = csv.reader(f, delimiter='\t')
 
     for row in tqdm(reader):
         (head, relation, tail) = row
-        TRAIN_ENTITIES.add(head)
-        TRAIN_ENTITIES.add(tail)
-        TRAIN_RELATIONS.add(relation)
-        TRAIN_TRIPLETS.append((head, relation, tail))
+        ENTITIES.add(head)
+        ENTITIES.add(tail)
+        RELATIONS.add(relation)
+        TRIPLETS.append((head, relation, tail))
 
-no_entities = len(TRAIN_ENTITIES)
-no_relations = len(TRAIN_RELATIONS)
-no_triplets = len(TRAIN_TRIPLETS)
+no_entities = len(ENTITIES)
+no_relations = len(RELATIONS)
+no_triplets = len(TRIPLETS)
 
 # Converting to list to make these data structures subscriptable
-ENTITIES = list(TRAIN_ENTITIES)
-RELATIONS = list(TRAIN_RELATIONS)
+ENTITIES = list(ENTITIES)
+RELATIONS = list(RELATIONS)
 
-VALID_ENTITIES = set()
-VALID_RELATIONS = set()
+# Get a rough estimate of the number of unique entities and relations in each subgraph
+entities_in_subgraph = int((SUBGRAPH_CHANGE_PERCENT / 100.0) * no_entities)
+relations_in_subgraph = no_relations
+
+print(f"Generating KG for major task first...")
+
+# Create major task dataset
+MAJOR_ENTITIES = list(np.random.choice(ENTITIES, size=entities_in_subgraph))
+MAJOR_RELATIONS = list(RELATIONS)
+
+TRAIN_TRIPLETS = []
+
+for (head, relation, tail) in tqdm(TRIPLETS):
+        # All entities and relations in this triplet belong to the 
+        if head in MAJOR_ENTITIES and tail in MAJOR_ENTITIES and relation in MAJOR_RELATIONS:
+            TRAIN_TRIPLETS.append((head, relation, tail))
+
+with open('new_train.txt', 'w') as f:
+        writer = csv.writer(f, delimiter ='\t',quotechar =',',quoting=csv.QUOTE_MINIMAL)
+        for (head, relation, tail) in TRAIN_TRIPLETS:
+            writer.writerow([head, relation, tail])
+
+# Now create validation and test set
+
 VALID_TRIPLETS = []
-with open('./data/FB15K-237/valid.txt', 'r') as f:
+with open('./data/FB15K-237/original_valid.txt', 'r') as f:
     reader = csv.reader(f, delimiter='\t')
 
     for row in tqdm(reader):
         (head, relation, tail) = row
-        if head in TRAIN_ENTITIES and relation in TRAIN_RELATIONS and tail in TRAIN_ENTITIES:
-            VALID_ENTITIES.add(head)
-            VALID_ENTITIES.add(tail)
-            VALID_RELATIONS.add(relation)
+        if head in MAJOR_ENTITIES and relation in MAJOR_RELATIONS and tail in MAJOR_ENTITIES:
             VALID_TRIPLETS.append((head, relation, tail))
 
 with open('new_valid.txt', 'w') as f:
@@ -45,18 +73,13 @@ with open('new_valid.txt', 'w') as f:
         for (head, relation, tail) in VALID_TRIPLETS:
             writer.writerow([head, relation, tail])
 
-TEST_ENTITIES = set()
-TEST_RELATIONS = set()
 TEST_TRIPLETS = []
-with open('./data/FB15K-237/test.txt', 'r') as f:
+with open('./data/FB15K-237/original_test.txt', 'r') as f:
     reader = csv.reader(f, delimiter='\t')
 
     for row in tqdm(reader):
         (head, relation, tail) = row
-        if head in TRAIN_ENTITIES and relation in TRAIN_RELATIONS and tail in TRAIN_ENTITIES:
-            TEST_ENTITIES.add(head)
-            TEST_ENTITIES.add(tail)
-            TEST_RELATIONS.add(relation)
+        if head in MAJOR_ENTITIES and relation in MAJOR_RELATIONS and tail in MAJOR_ENTITIES:
             TEST_TRIPLETS.append((head, relation, tail))
 
 with open('new_test.txt', 'w') as f:
